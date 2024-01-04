d7355ec3972ee3eb1b66d670f27a533039be454d
pallabofficial <officialpallabmondal000@gmail.com>
2024-01-04 20:21:07 +0530

    added a background color change feature:added a setTimeout method to change background color on a particular interval after the start button gets clicked

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
-const startButton = document.querySelector("#start-button");
-const stopButton = document.querySelector("#stop-button");
-function greet(){
-    console.log("hello");
+// index.js
+let startButton = document.querySelector("#start-button");
+let stopButton = document.querySelector("#stop-button");
+
+function randomNumber() {
+    return Math.floor(Math.random() * 255);
 }
+
+let body = document.querySelector("body");
+
+function changeBgColor() {
+    body.style.backgroundColor = `rgb(${randomNumber()}, ${255 - randomNumber()}, ${255 - randomNumber()})`;
+}
+
 let interval;
-startButton.addEventListener("click", function(){
-    interval = setInterval(greet,2000);
+
+body.addEventListener("click", function() {
+    interval = setInterval(changeBgColor, 2000);
 });
-stopButton.addEventListener("click", function(){
+
+stopButton.addEventListener("click", function() {
     clearInterval(interval);
 });
