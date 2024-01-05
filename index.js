5bd482a26bf773d38656d2e83a800ba5024f234e
pallabofficial <officialpallabmondal000@gmail.com>
2024-01-05 19:00:45 +0530

    fixed javascript index file:- Due to an error, the stop button event was not working, therefore fixed it.

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// index.js
 let startButton = document.querySelector("#start-button");
 let stopButton = document.querySelector("#stop-button");
 
@@ -6,18 +5,34 @@ function randomNumber() {
     return Math.floor(Math.random() * 255);
 }
 
-let body = document.querySelector("body");
+// let body = document.querySelector("body");
 
 function changeBgColor() {
-    body.style.backgroundColor = `rgb(${randomNumber()}, ${255 - randomNumber()}, ${255 - randomNumber()})`;
+    document.querySelector("body").style.backgroundColor = `rgb(${randomNumber()}, ${255 - randomNumber()}, ${255 - randomNumber()})`;
 }
 
 let interval;
 
-body.addEventListener("click", function() {
+startButton.addEventListener("click", function() {
     interval = setInterval(changeBgColor, 2000);
 });
 
 stopButton.addEventListener("click", function() {
     clearInterval(interval);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
